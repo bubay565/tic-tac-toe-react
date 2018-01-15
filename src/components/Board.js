ad14a56f803545e30718cfd988d23494855d6bf9
Buwa <bubay565@yahoo.com>
2018-01-15 22:14:57 +0000

    implemented functionality to determine winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Square from './Square'
+import calculateWinner from '../utils/helper'
 
 class Board extends Component {
   state = {
@@ -9,6 +10,11 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+    
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares,
@@ -26,7 +32,14 @@ class Board extends Component {
   };
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'} `;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'} `;
+    }
 
     return (
       <div>
