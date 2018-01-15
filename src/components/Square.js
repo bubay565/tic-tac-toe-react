405ab7ceea4d199d4f901e67bbb0070bee6d219b
Buwa <bubay565@yahoo.com>
2018-01-15 20:53:34 +0000

    toggle individual square value implemented

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 class Square extends Component {
+  state = {
+    value: null
+  }
+
   render() {
     return (
-      <button className="square">
-
+      <button className="square" onClick={() => this.setState({ value: 'X'})}>
+        {this.state.value}
       </button>
     )
   }
